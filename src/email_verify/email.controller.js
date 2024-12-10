1e39a47ea18f60cfd332c12457955ca98fd541d2
fhmianoor <149936677+fhmianoor@users.noreply.github.com>
2024-12-10 14:58:52 +0700

    verify email on otp

--- src/email_verify/email.controller.js
+++ src/email_verify/email.controller.js
@@ -1,4 +1,4 @@
-import { sendOtp } from "../otp/otp.controller.js";
+import { sendOtp, verifyOtp, deleteOtp } from "../otp/otp.controller.js";
 import User from "../models/user.js"
 
 export const verifyEmail = async (userEmail) => {
@@ -19,5 +19,20 @@ export const verifyEmail = async (userEmail) => {
 
     } catch (error) {
         throw new Error(error.message || "Failed to verify email");
+    }
+}
+
+export const verifyEmailOtp = async (email, otp) => {
+    try{
+        const validOtp = await verifyOtp(email, otp);
+
+        if(!validOtp){
+            throw new Error("Invalid OTP");
+        }
+
+        await deleteOtp(email);
+        return true;
+    }catch(error){
+        throw new Error(error.message || "Failed to verify email OTP");
     }
 }
--- src/email_verify/email.route.js
+++ src/email_verify/email.route.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { verifyEmail } from "./email.controller.js";
+import { verifyEmail, verifyEmailOtp } from "./email.controller.js";
 
 const router = express.Router();
 
@@ -12,6 +12,23 @@ router.post("/", async (req, res) => {
 
         const createdOtp = await verifyEmail(email);
         return res.status(200).json({ message: "Email verified successfully", createdOtp });
+    } catch (error) {
+        return res.status(500).json({ message: error.message });
+    }
+});
+
+router.post("/verify", async (req, res) => {
+    try {
+        let { email, otp } = req.body;
+
+        if(!email || !otp){
+            throw error("Email and OTP are required");
+        }
+
+        await verifyEmailOtp(email, otp);
+        return res.status(200).json({ message: "Email verified successfully" });
+    
+        
     } catch (error) {
         return res.status(500).json({ message: error.message });
     }
